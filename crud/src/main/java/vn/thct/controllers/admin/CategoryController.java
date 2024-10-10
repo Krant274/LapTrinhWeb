a9eaeec59159b234e502872cadebdfd8949046f3
Krant274 <thctan274@proton.me>
2024-10-10 22:58:53 +0700

    add upload file

--- crud/src/main/java/vn/thct/controllers/admin/CategoryController.java
+++ crud/src/main/java/vn/thct/controllers/admin/CategoryController.java
@@ -1,19 +1,24 @@
 package vn.thct.controllers.admin;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import vn.thct.impl.CategoryServiceImpl;
 import vn.thct.models.CategoryModel;
 import vn.thct.services.ICategoryService;
+import vn.thct.utils.Constant;
 
 
-
+@MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5)
 @WebServlet(urlPatterns = {"/admin/categories", "/admin/category/add", "/admin/category/insert", "/admin/category/edit", "/admin/category/update",
 		"/admin/category/delete", "/admin/category/search"})
 public class CategoryController extends HttpServlet {
@@ -49,28 +54,81 @@ public class CategoryController extends HttpServlet {
 		req.setCharacterEncoding("UTf-8");
 		resp.setCharacterEncoding("UTf-8");
 		if(url.contains("insert")) {
-			String categoryname = req.getParameter("categoryname");
-			String status = req.getParameter("status");
-			int statuss = Integer.parseInt(status);
-			String images ="https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_7__2_102.png";
 			CategoryModel category = new CategoryModel();
+			String categoryname = req.getParameter("categoryname");
+			int status = Integer.parseInt(req.getParameter("status"));
 			category.setCategoryname(categoryname);
-			category.setImages(images);
-			category.setStatus(statuss);
+			category.setStatus(status);
+			
+			String fname ="";
+			String uploadPath = Constant.DIR;
+			File uploadDir = new File(uploadPath);
+			if(!uploadDir.exists()) {
+				uploadDir.mkdir();
+			}
+			try {
+				Part part =req.getPart("images");
+				if(part.getSize()>0) {
+					String filename = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+					// rename
+					int index = filename.lastIndexOf(".");
+					String ext = filename.substring(index+1);
+					fname = System.currentTimeMillis() + "." + ext;
+					//upload
+					part.write(uploadPath+ "/" + fname);
+					// write to database
+					category.setImages(fname);
+				} else {
+					category.setImages("avatar.png");
+				}
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			
 			cateService.insert(category);
 			resp.sendRedirect(req.getContextPath() + "/admin/categories");
 		}
 		else if (url.contains("update")){
+			CategoryModel category = new CategoryModel();
 			int categoryid = Integer.parseInt(req.getParameter("categoryid"));
 			String categoryname = req.getParameter("categoryname");
 			String status = req.getParameter("status");
 			int statuss = Integer.parseInt(status);
-			String images ="https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_7__2_102.png";
-			CategoryModel category = new CategoryModel();
 			category.setCategoryid(categoryid);
 			category.setCategoryname(categoryname);
-			category.setImages(images);
 			category.setStatus(statuss);
+			
+			CategoryModel cateOld = cateService.findById(categoryid);
+			String fileOld = cateOld.getImages();
+			
+			
+			
+			String fname ="";
+			String uploadPath = Constant.DIR;
+			File uploadDir = new File(uploadPath);
+			if(!uploadDir.exists()) {
+				uploadDir.mkdir();
+			}
+			try {
+				Part part =req.getPart("images");
+				if(part.getSize()>0) {
+					String filename = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+					// rename
+					int index = filename.lastIndexOf(".");
+					String ext = filename.substring(index+1);
+					fname = System.currentTimeMillis() + "." + ext;
+					//upload
+					part.write(uploadPath+ "/" + fname);
+					// write to database
+					category.setImages(fname);
+				} else {
+					category.setImages(fileOld);
+				}
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 			
 			cateService.update(category);
 			resp.sendRedirect(req.getContextPath() + "/admin/categories");
